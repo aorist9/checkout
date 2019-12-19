129cdd4c339f3f1b7555cd1fc3a694c69c5ff79e
Andrew Rist <aorist9@gmail.com>
2019-12-18 21:13:10 -0600

    feature: added Apple class

--- src/main/java/checkout/types/Item.java
+++ src/main/java/checkout/types/Item.java
@@ -3,39 +3,35 @@ package checkout.types;
 import java.math.BigDecimal;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.function.Supplier;
+
+import checkout.types.Apple;
 
 /**
  * Item - represents an item to be bought, abstract rather than an interface
  * since it represents a thing rather than indicating a behavior
  */
 public abstract class Item {
-   private static Map<String, Supplier<Item>> itemRegistry = new HashMap<>();
+   private static Map<String, Supplier<Item>> itemMap = new HashMap<>();
 
-   /**
-    * registerItem - a static method used to register new subclasses of item with
-    * their names as strings, so that those new subclasses can be created from their
-    * strings without having to alter the Item class
-    * @param name the name of the item
-    * @param itemSupplier a Supplier (preferably a lambda) did instantiates a
-    *                     new Item (not an item, because that would result in
-    *                     all Apples being the same Apple object
-    */
-   public static void registerItem(String name, Supplier<Item> itemSupplier) {
-      itemRegistry.put(name, itemSupplier);
+   static {
+      itemMap.put("APPLE", () -> new Apple());
    }
 
    /**
     * getItem - a factory method to generate an Item from a String
     * @param name the name of the Item
     * @return an Item object corresponding to the name
+    * @throws NullPointerException if name is null
     */
    public static Item getItem(String name) {
-      if (itemRegistry.get(name) == null) {
+      String key = Objects.requireNonNull(name).toUpperCase();
+      if (itemMap.get(key) == null) {
          System.out.println("Unrecognized item " + name + " requested");
 	 return null;
       } else {
-         return itemRegistry.get(name).get();
+         return itemMap.get(key).get();
       }
    }
 
