344157b7df486b464fc43ffdb14d6bb96bd8a7f2
Andrew Rist <aorist9@gmail.com>
2019-12-19 19:11:31 -0600

    chore: added to javadocs

--- src/main/java/checkout/CheckoutApplication.java
+++ src/main/java/checkout/CheckoutApplication.java
@@ -10,6 +10,14 @@ public class CheckoutApplication {
    /**
     * main - takes the items to be purchased as command line arguments and prints
     * the price of the cart
+    *
+    * Note: I know that it would be simpler to loop with a switch statement that adds
+    * to a sum, but while not being aware of the second step, I know that there is a
+    * second step (akin to knowing there are future improvements to a system on the
+    * horizon); given that, I opted to add a couple of files worth of complexity to
+    * make the code more extensible and maintainable (at the risk of violating the
+    * YAGNI principle.
+    *
     * @param args the array of command line arguments, which will be mapped to items
     */
    public static void main(String[] args) {
